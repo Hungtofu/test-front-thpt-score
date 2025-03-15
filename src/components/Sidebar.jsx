c64b78e78a56ae360bcf9a3faed741fe7b1fdf0b
Hungtofu <vinhhungalex03@gmail.com>
2025-03-15 17:34:09 +0700

    make it better 2.0

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,11 +6,9 @@ const Sidebar = () => {
     <div className="h-screen w-auto bg-gradient-to-b from-yellow-500 to-green-600 text-white p-5">
       <h2 className="text-xl font-bold mb-5 text-center text-black">Menu</h2>
       <ul className="space-y-3">
-        <li><Link to="/" className="block hover:text-gray-200 text-black">Dashboard</Link></li>
         <li><Link to="/search" className="block hover:text-gray-200 text-black">Search Scores</Link></li>
         <li><Link to="/report" className="block hover:text-black text-black">Reports</Link></li>
         <li><Link to="/top" className="block hover:text-black text-black">Top Group A</Link></li>
-        <li><Link to="/" className="">Setting</Link></li>
       </ul>
     </div>
   );
